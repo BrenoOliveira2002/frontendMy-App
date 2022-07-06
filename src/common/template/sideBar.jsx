105429a70efa30b0ae44a2c869fd28961ce42db2
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-06 12:41:31 -0300

    Menu Component

---
+++ src/common/template/sideBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Menu from "./menu"
+
+export default props => (
+
+    <aside className="main-sidebar">
+        <section className="side-bar">
+            <Menu></Menu>
+        </section>
+    </aside>
+)

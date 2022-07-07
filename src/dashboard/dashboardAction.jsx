61a2ef1271aa19beac37342bdeefafb428c39a20
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-07 12:07:40 -0300

    configuring redux DEVTOOLS

--- src/dashboard/dashboardAction.jsx
+++ src/dashboard/dashboardAction.jsx
@@ -6,7 +6,6 @@ export function getSummary() {
     const request = axios.get(`${BASE_URL}/billingCycles/summary`)
     return{
         type: 'BILLING_SUMMARY_FETCHED',
-        payload: request 
-        
+        payload: request  
     }
 }

65e068ad1f5fd0a368067b110614d1e68f6a0f18
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-07 08:25:02 -0300

    footer component

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default props => (
+
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2022
+            <a href="http://brenooliveira.com.br" target='_blank'> Breno </a> 
+        </strong> 
+    </footer>
+)

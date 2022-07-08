7dd98f8eb222f13bb875d55db8500fe203c2a759
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-08 10:29:25 -0300

    tab visibility #2

--- src/common/tab/TabContent.jsx
+++ src/common/tab/TabContent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import If from '../operator/if'
 
 
 class TabContent extends Component {
@@ -8,15 +9,18 @@ class TabContent extends Component {
     render() {
 
         const Selected = this.props.tab.selected === this.props.id
+        const visible = this.props.visible[this.props.id]
 
         return (
-            <div id={this.props.id}
-            className={`tab-pane ${selected ? 'active' : ''}`}>
-                {this.props.children}
-            </div>
+            <If test={visible}>
+                <div id={this.props.id}
+                    className={`tab-pane ${selected ? 'active' : ''}`}>
+                    {this.props.children}
+                </div>
+            </If>
         )
     }
 }
 
-const mapStateToPros = state => ({tab: state.tab})
+const mapStateToPros = state => ({ tab: state.tab })
 export default connect(mapStateToProps)(TabContent)
--- src/common/tab/tabHeader.jsx
+++ src/common/tab/tabHeader.jsx
@@ -1,29 +1,33 @@
-import React, {Component} from "react";
-import { bindActionCreators} from 'redux'
+import React, { Component } from "react";
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-import {selectTab } from './tabActions'
+import If from '../operator/if'
+import { selectTab } from './tabActions'
 
 class TabsHeader extends Component {
 
     render() {
 
         const selected = this.props.tab.selected === this.props.target
+        const visible = this.props.visible[this.props.target]
 
         return (
 
-            <li className={selected ? 'active' : '' }>
-                <a href ='javascript:;'
-                data-toggle='tab'
-                onClick={() => this.props.selectTab(this.props.target)}
-                data-target={this.props.target}>
-                    <i className={`fa fa-${this.props.icon}`}></i>{this.props.label}
-                </a>
-            </li>
+            <If test={visible}>
+                <li className={selected ? 'active' : ''}>
+                    <a href='javascript:;'
+                        data-toggle='tab'
+                        onClick={() => this.props.selectTab(this.props.target)}
+                        data-target={this.props.target}>
+                        <i className={`fa fa-${this.props.icon}`}></i>{this.props.label}
+                    </a>
+                </li>
+            </If>
         )
     }
 }
 
-const mapToStateToProps = state => ({tab : state.tab})
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+const mapToStateToProps = state => ({ tab: state.tab })
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
 export default connect(mapToStateToProps, mapDispatchToProps)(TabsHeader)

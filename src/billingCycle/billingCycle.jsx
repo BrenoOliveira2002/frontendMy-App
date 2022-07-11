ef648d71f90adea7ee23ab2c0bec8dd96ab9256d
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-11 12:24:58 -0300

    finalizing the basic registration

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -42,13 +42,16 @@ class BillingCycle extends Component {
                                 <List></List>
                             </TabContent>
                             <TabContent id='tabCreate'>
-                                <Form onSubmit={this.props.create}></Form> 
+                                <Form onSubmit={this.props.create}
+                                    submitLabel="Incluir" submitClass='primary' /> 
                             </TabContent>
                             <TabContent id='tabUpdate'>
-                                <Form onSubmit={this.props.update} />
-                            </TabContent> 
+                                <Form onSubmit={this.props.update}
+                                    submitLabel='Alterar' submitClass='info' />
+                            </TabContent>
                             <TabContent id='tabDelete'>
-                                <Form onSubmit={this.props.remove} readOnly={true} />
+                                <Form onSubmit={this.props.remove} readOnly={true}
+                                    submitLabel='Excluir' submitClass='danger' />
                             </TabContent> 
                         </TabsContent>
                     </Tabs>

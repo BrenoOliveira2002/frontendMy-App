448499875fb6ea5aaad47a2074f13e596206d165
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-12 09:43:40 -0300

    managing the creditlist component

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -5,7 +5,7 @@ import { reduxForm, Field , formValueSelector} from 'redux-form'
 
 import { init } from './billingCycleActions'
 import labelAndInput from '../common/form/labelAndInput'
-import CreditList from './creditList'
+import ItemList from './itemList'
 
 class BillingCycleForm extends Component {
 
@@ -24,7 +24,9 @@ class BillingCycleForm extends Component {
 
                     <Field year='year' component={labelAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano' />
-                        <CreditList cols='12 6' list={credits} readOnly={readOnly}/>
+
+                        <ItemList cols='12 6' list={credits} readOnly={readOnly}
+                        field='credits' legend='Creditos' />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>
